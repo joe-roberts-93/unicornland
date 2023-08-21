f832d60b0e918ab2ac7f6fd954e154730b70677a
Sebastien Fournier <sfournier.qc@gmail.com>
2023-08-21 17:14:10 -0400

    changing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,11 @@ price = [1000, 50, 100_000, 25, 5000, 500, 10_000]
 type = ["bulky", "Fat", "Hunted", "Chubby", "Baby", "Cute", "Barbie"]
 
 Unicorn.create!(name: "joe", gender: "male", age: 12, color: "gold", price: "1_000_000", type: "fat")
-Unicorn.create!(name: "priscillia", gender: "female", age: 1, color: "gold", price: "1_000_000", type: "business")
+Unicorn.create!(name: "priscilla", gender: "female", age: 1, color: "gold", price: "1_000_000", type: "business")
 Unicorn.create!(name: "daphne", gender: "female", age: 1, color: "gold", price: "1_000_000", type: "barbie")
 Unicorn.create!(name: "sebastien", gender: "male", age: 7, color: "gold", price: "1_000_000", type: "bulky")
 
+Unicorn.create!(name: "fred", gender: gender.sample, age: age.sample, color: color.sample, price: price.sample, type: type.sample)
 Unicorn.create!(name: "justin", gender: gender.sample, age: age.sample, color: color.sample, price: price.sample, type: type.sample)
 Unicorn.create!(name: "marcelo", gender: gender.sample, age: age.sample, color: color.sample, price: price.sample, type: type.sample)
 Unicorn.create!(name: "audrey", gender: gender.sample, age: age.sample, color: color.sample, price: price.sample, type: type.sample)
