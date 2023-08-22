065586a6d6dd18d9bfba908efb190477fed95df7
PRISCILLA URIBE <puribe0@student.ubc.ca>
2023-08-22 14:32:27 -0400

    show page created

--- app/controllers/unicorns_controller.rb
+++ app/controllers/unicorns_controller.rb
@@ -2,4 +2,22 @@ class UnicornsController < ApplicationController
   def index
     @unicorns = Unicorn.all
   end
+
+  # def new
+  #   @unicorns = Unicorn.new
+  # end
+
+  def show
+    @unicorn = Unicorn.find(params[:id])
+  # TODO: improve logic to recommend more relevant unicorns
+    @unicorns = Unicorn.first(3)
+  end
+
+
+
+private
+
+  def unicorn_params
+    params.require(:unicorn_id).permit(:id, :gender, :age, :color, :price, :variety)
+  end
 end
