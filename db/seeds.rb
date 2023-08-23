452703a4063a4d0db360ae60ecf45d6a169cfe6c
Sebastien Fournier <sfournier.qc@gmail.com>
2023-08-23 12:07:40 -0400

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Reservation.destroy_all
 Unicorn.destroy_all
 User.destroy_all
 
@@ -15,13 +16,13 @@ color = ["blue", "pink", "orange", "gray", "purple", "silver", "bronze"]
 price = [1000, 50, 100_000, 25, 5000, 500, 10_000]
 variety = ["bulky", "Fat", "Haunted", "Chubby", "Baby", "Cute", "Barbie"]
 
-User.create!(first_name: "Sébastien", last_name: "Fournier", email: "seb@email.com", password: "secret")
+User.create!(first_name: "Sebastien", last_name: "Fournier", email: "seb@email.com", password: "secret")
 owner = User.create!(first_name: "Joe", last_name: "Roberts", email: "joe@email.com", password: "secret", owner: true)
 
-Unicorn.create!(name: "joe", gender: "male", age: 12, color: "gold", price: "1_000_000", variety: "fat", user: owner)
-Unicorn.create!(name: "priscilla", gender: "female", age: 1, color: "gold", price: "1_000_000", variety: "business", user: owner)
-Unicorn.create!(name: "daphne", gender: "female", age: 1, color: "gold", price: "1_000_000", variety: "barbie", user: owner)
-Unicorn.create!(name: "seb", gender: "male", age: 7, color: "gold", price: "1_000_000", variety: "bulky", user: owner)
+Unicorn.create!(name: "joe", gender: "male", age: 12, color: "gold", price: "1_000_000", variety: "fat", team_member: true, user: owner, )
+Unicorn.create!(name: "priscilla", gender: "female", age: 1, color: "gold", price: "1_000_000", variety: "business", team_member: true, user: owner)
+Unicorn.create!(name: "daphne", gender: "female", age: 1, color: "gold", price: "1_000_000", variety: "barbie", team_member: true, user: owner)
+Unicorn.create!(name: "seb", gender: "male", age: 7, color: "gold", price: "1_000_000", variety: "bulky", team_member: true, user: owner)
 
 Unicorn.create!(name: "fred", gender: gender.sample, age: age.sample, color: color.sample, price: price.sample, variety: variety.sample, user: owner)
 Unicorn.create!(name: "justin", gender: gender.sample, age: age.sample, color: color.sample, price: price.sample, variety: variety.sample, user: owner)
