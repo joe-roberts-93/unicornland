119d665240da54b71b1c043c608882983ff40e93
Joe Roberts <roberts.josephpaul@gmail.com>
2023-08-24 17:29:44 -0400

    added seeds for 10 unicorns and 2 users

--- app/models/unicorn.rb
+++ app/models/unicorn.rb
@@ -1,5 +1,5 @@
 class Unicorn < ApplicationRecord
-  VARIETY = ["Bulky", "Fat", "Haunted", "Business", "Baby", "Cute", "Barbie"].freeze
+  VARIETY = ["Bulky", "Fat", "Haunted", "Business", "Murder", "Cute", "Barbie"].freeze
 
   belongs_to :user
   has_many :reservations
