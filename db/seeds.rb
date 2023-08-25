4cef018170e85200f698d770b38d5a32ccbf4fdb
Joe Roberts <roberts.josephpaul@gmail.com>
2023-08-25 11:02:44 -0400

    added custom seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@ priscilla = Unicorn.new(name: "priscilla", gender: "female", age: 1, color: "gol
 priscilla.photo.attach(io: priscilla_photo, filename: "priscilla.jpg", content_type: "image/jpg")
 priscilla.save
 
-daphne_photo = URI.open("https://i.ibb.co/hV9SCmM/DALL-E-2023-08-24-15-04-48-A-photo-of-a-golden-barbie-unicorn-figurine.png")
+daphne_photo = URI.open("https://th.bing.com/th/id/OIG.uoxrChBPPVM9AvFP_xzH?pid=ImgGn")
 daphne = Unicorn.new(name: "daphne", gender: "female", age: 1, color: "gold", price: "1_000_000", variety: "Barbie", team_member: true, user: owner)
 daphne.photo.attach(io: daphne_photo, filename: "daphne.png", content_type: "image/png")
 daphne.save
@@ -36,32 +36,51 @@ seb.photo.attach(io: seb_photo, filename: "seb.png", content_type: "image/jpg")
 seb.save
 
 fred_photo = URI.open("https://th.bing.com/th/id/OIG.3VRzSgpCtXBvjW.05yQN?pid=ImgGn")
-fred = Unicorn.new(name: "fred", gender: "male", age: 11, color: "white", price: "500_000", variety: "Murder", team_member: true, user: owner)
+fred = Unicorn.new(name: "fred", gender: "male", age: 11, color: "white", price: "500_000", variety: "Murder", team_member: false, user: owner)
 fred.photo.attach(io: fred_photo, filename: "fred.png", content_type: "image/jpg")
 fred.save
 
 marcela_photo = URI.open("https://th.bing.com/th/id/OIG.Ib8Udc.5FfCbKRDL.Rz_?pid=ImgGn")
-marcela = Unicorn.new(name: "marcela", gender: "female", age: 5, color: "white", price: "250_000", variety: "Cute", team_member: true, user: owner)
+marcela = Unicorn.new(name: "marcela", gender: "female", age: 5, color: "white", price: "250_000", variety: "Cute", team_member: false, user: owner)
 marcela.photo.attach(io: marcela_photo, filename: "marcela.png", content_type: "image/jpg")
 marcela.save
 
 grace_photo = URI.open("https://th.bing.com/th/id/OIG.g99oemtIGtj17P7RyC8H?pid=ImgGn")
-grace = Unicorn.new(name: "grace", gender: "female", age: 3, color: "white", price: "300_000", variety: "Haunted", team_member: true, user: owner)
+grace = Unicorn.new(name: "grace", gender: "female", age: 3, color: "white", price: "300_000", variety: "Haunted", team_member: false, user: owner)
 grace.photo.attach(io: grace_photo, filename: "grace.png", content_type: "image/jpg")
 grace.save
 
 audrey_photo = URI.open("https://th.bing.com/th/id/OIG.Ys2fSCg4lA8SF.AAQDNs?pid=ImgGn")
-audrey = Unicorn.new(name: "audrey", gender: "female", age: 6, color: "pink", price: "20_000", variety: "Cute", team_member: true, user: owner)
+audrey = Unicorn.new(name: "audrey", gender: "female", age: 6, color: "pink", price: "20_000", variety: "Cute", team_member: false, user: owner)
 audrey.photo.attach(io: audrey_photo, filename: "audrey.png", content_type: "image/jpg")
 audrey.save
 
 david_photo = URI.open("https://th.bing.com/th/id/OIG.Ry9IM5IPtX30dGHwpNql?pid=ImgGn")
-david = Unicorn.new(name: "david", gender: "female", age: 6, color: "white", price: "10_000", variety: "Haunted", team_member: true, user: owner)
+david = Unicorn.new(name: "david", gender: "female", age: 9, color: "white", price: "10_000", variety: "Murder", team_member: false, user: owner)
 david.photo.attach(io: david_photo, filename: "david.png", content_type: "image/jpg")
 david.save
 
-https://th.bing.com/th/id/OIG.OBV21STN_e_tiWqVnrrp?pid=ImgGn
 sergio_photo = URI.open("https://th.bing.com/th/id/OIG.OBV21STN_e_tiWqVnrrp?pid=ImgGn")
-sergio = Unicorn.new(name: "sergio", gender: "female", age: 6, color: "green", price: "50_000", variety: "Cute", team_member: true, user: owner)
+sergio = Unicorn.new(name: "sergio", gender: "male", age: 5, color: "green", price: "50_000", variety: "Cute", team_member: false, user: owner)
 sergio.photo.attach(io: sergio_photo, filename: "sergio.png", content_type: "image/jpg")
 sergio.save
+
+justin_photo = URI.open("https://th.bing.com/th/id/OIG.7nMotTxOACoY7h9p.wAW?pid=ImgGn")
+justin = Unicorn.new(name: "justin", gender: "male", age: 10, color: "white", price: "70_000", variety: "Haunted", team_member: false, user: owner)
+justin.photo.attach(io: justin_photo, filename: "justin.png", content_type: "image/jpg")
+justin.save
+
+grant_photo = URI.open("https://th.bing.com/th/id/OIG._Qi221igjDMGxTJhyJZf?pid=ImgGn")
+grant = Unicorn.new(name: "grant", gender: "male", age: 3, color: "pink", price: "15_000", variety: "Barbie", team_member: false, user: owner)
+grant.photo.attach(io: grant_photo, filename: "grant.png", content_type: "image/jpg")
+grant.save
+
+kitty_photo = URI.open("https://th.bing.com/th/id/OIG.hw2yYSazUW2gwQ24VCIW?pid=ImgGn")
+kitty = Unicorn.new(name: "kitty", gender: "female", age: 3, color: "pink", price: "15_000", variety: "Haunted", team_member: false, user: owner)
+kitty.photo.attach(io: kitty_photo, filename: "kitty.png", content_type: "image/jpg")
+kitty.save
+
+marcelo_photo = URI.open("https://th.bing.com/th/id/OIG.Fb9QoL95Cez4gEecAgvI?pid=ImgGn")
+marcelo = Unicorn.new(name: "marcelo", gender: "male", age: 3, color: "pink", price: "15_000", variety: "Bulky", team_member: false, user: owner)
+marcelo.photo.attach(io: marcelo_photo, filename: "marcelo.png", content_type: "image/jpg")
+marcelo.save
